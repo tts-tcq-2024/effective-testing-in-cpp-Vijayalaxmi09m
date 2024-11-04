ff3218295d3633c0bd7ec61bb730b312b1464e56
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-11-04 20:03:40 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -2,15 +2,16 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
     } else if(cms > 42) {
         sizeName = 'L';
     }
     return sizeName;
 }
+
 int main() {
     return 0;
 }
