e5cb36546fb6df8e6bc53856458ad0a5ca10f160
Vijayalaxmi09m <46927317+Vijayalaxmi09m@users.noreply.github.com>
2024-11-04 20:05:45 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,18 +1,13 @@
 #include "tshirts.h"
 
-char size(int cms) {
+char size(float cms) {  // Change int to float
     char sizeName = '\0';
-    if(cms <= 38) {
+    if (cms <= 38.0) {
         sizeName = 'S';
-    } else if(cms > 38 && cms <= 42) {
+    } else if (cms > 38.0 && cms <= 42.0) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if (cms > 42.0) {
         sizeName = 'L';
     }
     return sizeName;
 }
-
-int main() {
-    return 0;
-}
-
